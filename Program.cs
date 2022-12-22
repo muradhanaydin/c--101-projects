f366e337b83742b05dc687a49812636684501e26
MURADHAN AYDIN <muradhanaydin@gmail.com>
2022-12-23 02:22:44 +0300

    work 2 added

--- Program.cs
+++ Program.cs
@@ -6,14 +6,25 @@ namespace conosole1 // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            float purchasePrice , totalPrice , rate;
-            Console.Write("Alis fiyati giriniz: ");
-            purchasePrice = float.Parse(Console.ReadLine());
-            Console.Write("Kar orani giriniz (%): ");
-            rate = float.Parse(Console.ReadLine()); 
-            totalPrice = purchasePrice +(purchasePrice* rate / 100);
-            Console.WriteLine($"Satis Fiyatiniz : {totalPrice}TL");
+            /* ODEV 1
+                float purchasePrice , totalPrice , rate;
+                Console.Write("Alis fiyati giriniz: ");
+                purchasePrice = float.Parse(Console.ReadLine());
+                Console.Write("Kar orani giriniz (%): ");
+                rate = float.Parse(Console.ReadLine()); 
+                totalPrice = purchasePrice +(purchasePrice* rate / 100);
+                Console.WriteLine($"Satis Fiyatiniz : {totalPrice}TL");
+            */
 
+            Console.Write("Kullanici adi: ");
+            string kadi = Console.ReadLine();
+            Console.Write("Sifre: ");
+            string sifre = Console.ReadLine();
+            if(kadi == "admin" && sifre == "root"){
+                Console.WriteLine("Hosgeldiniz...");
+            }else{
+                Console.WriteLine("Sifre yada Kullanici Adi Hatali!");
+            }
         }
     }
 }

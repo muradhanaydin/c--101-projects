1022b9254c74ec2b58d508d4e491380cabca303a
Muradhan Aydın <52311599+muradhanaydin@users.noreply.github.com>
2023-01-30 21:47:55 +0300

    Add files via upload

---
+++ Silent-chars.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace c__101_proj{
+    internal class Program{
+        public static void Main(){
+           List<string> inp = Console.ReadLine().Split(" ").ToList<string>();
+           string vowels = "aeıioöuüAEIİOÖUÜ";
+           foreach(string text in inp){
+                bool stat = false;
+                for(int i=0; i<text.Length-1; i++){
+                        if(vowels.IndexOf(text[i]) == -1 && vowels.IndexOf(text[i+1]) == -1){
+                            stat = true;
+                            break;
+                        }
+                }
+                Console.Write($"{stat} ");
+           }
+        }
+    }
+}

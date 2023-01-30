94d0faf9b47d0b559a1ed939176414d434d5625d
Muradhan Aydın <52311599+muradhanaydin@users.noreply.github.com>
2023-01-30 21:19:12 +0300

    Add files via upload

---
+++ Absolute-squaring.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace c__101_proj{
+    internal class Program{
+        public static void Main(){
+           List<string> arr = Console.ReadLine().Split(" ").ToList<string>();
+           int minTotal = 0  , maxTotal = 0 , limit = 67;
+           for(int i=0; i<arr.Count; i++){
+                int number = int.Parse(arr[i]);
+                if(number < limit){
+                    minTotal += limit - number;
+                }else{
+                    maxTotal += Convert.ToInt16(Math.Pow(Math.Abs(limit - number),2));
+                }
+           }
+           Console.WriteLine($"{minTotal} {maxTotal}");
+        }        
+    }
+}

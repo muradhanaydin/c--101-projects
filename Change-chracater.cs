0fed99376bb2c1ec6db37266a5da246cf29fa192
Muradhan Aydın <52311599+muradhanaydin@users.noreply.github.com>
2023-01-30 21:37:47 +0300

    Add files via upload

---
+++ Change-chracater.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace c__101_proj{
+    internal class Program{
+        public static void Main(){
+           List<string> inp = Console.ReadLine().Split(" ").ToList<string>();
+           foreach(string text in inp){
+                if(text.Length != 1)
+                    Console.Write($"{text[text.Length-1] + text.Substring(1,text.Length-2) + text[0] } ");
+                else
+                    Console.Write($"{text} ");
+           }
+        }        
+    }
+}

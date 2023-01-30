f4ef27d6d65108d1222abed202f1653aadd4c57d
Muradhan Aydın <52311599+muradhanaydin@users.noreply.github.com>
2023-01-30 15:20:33 +0300

    Add files via upload

---
+++ Print-Triangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace c__101_proj{
+    internal class Program{
+        public static void Main(){
+            int size = int.Parse(Console.ReadLine());
+            PrintTriangle(size);
+        }
+
+        public static void PrintTriangle(int s){
+            for(int i=0; i<s; i++){
+                for(int j=i; j<s; j++){
+                    Console.Write(" ");
+                }
+                for(int c=0; c<i; c++){
+                    Console.Write("· ");
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        
+    }
+}

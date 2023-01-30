45f77d37472ed8e08a3b448a6c976fafc9f831c0
Muradhan Aydın <52311599+muradhanaydin@users.noreply.github.com>
2023-01-30 15:04:44 +0300

    Add files via upload

---
+++ Fibonacci-Avarage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace c__101_proj{
+    internal class Program{
+        public static void Main(){
+            int deep = int.Parse(Console.ReadLine());
+            float avg = fibonacciAvg(deep);
+            Console.Write($"{avg:F3}");
+        }
+
+        public static float fibonacciAvg(int d){
+            if(d < 3){
+                return 1;
+            }
+            List<int> fib = new List<int>(){1,1};
+            int total = 2 , curr = 0;
+            for(int i=2; i<d;i++){
+                curr = fib[i-1] + fib[i-2];
+                fib.Add(curr);
+                total += curr;
+            }
+            float avg = total /= d;
+            return avg;
+        }
+    }
+}

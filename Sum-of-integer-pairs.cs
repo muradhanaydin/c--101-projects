7ff59abba786f6512ed4706cb08af7b7c4717425
Muradhan Aydın <52311599+muradhanaydin@users.noreply.github.com>
2023-01-30 21:05:18 +0300

    Add files via upload

---
+++ Sum-of-integer-pairs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace c__101_proj{
+    internal class Program{
+        public static void Main(){
+           List<string> arr = Console.ReadLine().Split(" ").ToList<string>();
+           for(int i=0; i<arr.Count; i+=2){
+                if(arr[i] == arr[i+1]){
+                    Console.Write($"{Math.Pow((int.Parse(arr[i]) + int.Parse(arr[i+1])),2)}");
+                    continue;
+                }
+                Console.Write($"{(int.Parse(arr[i]) + int.Parse(arr[i+1]))}");
+           }
+        }        
+    }
+}

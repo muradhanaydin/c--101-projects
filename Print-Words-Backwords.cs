73f57080c13c1d14ba50c87fb986b5a121e0b7b5
Muradhan Aydın <52311599+muradhanaydin@users.noreply.github.com>
2023-01-30 15:59:34 +0300

    Add files via upload

---
+++ Print-Words-Backwords.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace c__101_proj{
+    internal class Program{
+        public static void Main(){
+            string[] words = Console.ReadLine().Split(" ");
+            PrintWordsBackwords(words);
+        }
+        public static void PrintWordsBackwords(string[] words){
+            foreach(string word in words){
+                Console.Write($"{string.Concat(word.ToCharArray().Reverse())} ");
+            }
+        }
+        
+    }
+}

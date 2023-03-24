35c1e52faf3abc2f9467bd73f4fb9e8a131bf2f1
Muradhan Aydın <52311599+muradhanaydin@users.noreply.github.com>
2023-03-25 00:19:47 +0300

    Adding Mapper

--- API/BookOperations/GetBookByIdQuery.cs
+++ API/BookOperations/GetBookByIdQuery.cs
@@ -1,26 +1,24 @@
 using api.Common;
 using api.DBOperations;
 using api.Modal;
+using AutoMapper;
 
 namespace api.BookOperations
 {
     public class GetBookByIdQuery
     {
         private readonly BookStoreDbContext _context;
-        public GetBookByIdQuery(BookStoreDbContext _context){
+        private readonly IMapper _mapper;
+        public GetBookByIdQuery(BookStoreDbContext _context , IMapper _mapper){
             this._context = _context;
+            this._mapper = _mapper;
         }
 
         public BookViewModel Handle(string id)
         {
             var result = _context.Books.Where(b => b.Id == int.Parse(id)).SingleOrDefault();
-            return new BookViewModel(){
-                Title = result.Title, 
-                Category = ((CategoryEnum)result.Category).ToString(),
-                PublishedDate = result.PublishDate.Date.ToString("dd/MM/yyy"),
-                Publisher = result.Publisher,
-                PageCount = result.PageCount
-            };
+            BookViewModel vm = _mapper.Map<BookViewModel>(result);
+            return vm;
         }
     }
 }

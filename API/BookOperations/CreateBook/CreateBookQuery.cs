27dfb2d7081187423290bf9d210981c3d5363d80
Muradhan Aydın <52311599+muradhanaydin@users.noreply.github.com>
2023-03-25 00:15:59 +0300

    Added Mapper And FluentValidation

--- API/BookOperations/CreateBook/CreateBookQuery.cs
+++ API/BookOperations/CreateBook/CreateBookQuery.cs
@@ -1,29 +1,37 @@
 using api.DBOperations;
+using AutoMapper;
 
 namespace api.BookOperations.CreateBook
 {
     public class CreateBookQuery
     {
-        private BookStoreDbContext _context;
-        public CreateBookQuery(BookStoreDbContext _context)
+        private readonly BookStoreDbContext _context;
+        private readonly IMapper _mapper;
+        public CreateBookQuery(BookStoreDbContext _context , IMapper _mapper)
         {
             this._context = _context;
+            this._mapper = _mapper;
         }
         public void Handle(CreateBookViewModal modal)
         {
-            Console.WriteLine(modal.Title);
-            var book = _context.Books.SingleOrDefault(b => b.Title == modal.Title);
-            if(book is not null){
-                throw new InvalidOperationException($"{book.Title} isimli kitap zaten kayitli! Kitap ID: {book.Id}");
+            CreateBokQueryValidator validater = new CreateBokQueryValidator();
+            var result = validater.Validate(modal);
+            if(result.IsValid)
+            {
+                var book = _context.Books.SingleOrDefault(b => b.Title == modal.Title);
+                if(book is not null){
+                    throw new InvalidOperationException($"{book.Title} isimli kitap zaten kayitli! Kitap ID: {book.Id}");
+                }
+                book = _mapper.Map<Book>(modal);
+                _context.Books.Add(book);
+                _context.SaveChanges();
+            }else{
+                string errorMessage = "";
+                result.Errors.ForEach(error => {
+                    errorMessage += error.ErrorMessage+"\n";
+                });
+                throw new InvalidOperationException(errorMessage);
             }
-            book = new Book();
-            book.Title = modal.Title;
-            book.Category = modal.Category;
-            book.PageCount = modal.PageCount;
-            book.PublishDate = modal.PublishDate;
-            book.Publisher = modal.Publisher;
-            _context.Books.Add(book);
-            _context.SaveChanges();
         }
     }
 }

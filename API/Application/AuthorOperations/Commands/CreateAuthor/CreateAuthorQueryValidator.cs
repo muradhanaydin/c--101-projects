5fab5a3f3f790b3dd5dddbf9d82a979ccb7d7c3d
Muradhan Aydın <52311599+muradhanaydin@users.noreply.github.com>
2023-04-16 14:26:33 +0300

    Validator Type has been changed query from model.

--- API/Application/AuthorOperations/Commands/CreateAuthor/CreateAuthorQueryValidator.cs
+++ API/Application/AuthorOperations/Commands/CreateAuthor/CreateAuthorQueryValidator.cs
@@ -2,13 +2,13 @@ using FluentValidation;
 
 namespace api.Application.AuthorOperations.Commdans.CreateAuthor
 {
-    public class CreateAuthorQueryValidator : AbstractValidator<CreateAuthorQuery>
+    public class CreateAuthorQueryValidator : AbstractValidator<CreateAuthorQueryModel>
     {
         public CreateAuthorQueryValidator()
         {
-            RuleFor(query => query.Model.Name).NotEmpty().MinimumLength(2);
-            RuleFor(query => query.Model.Surname).NotEmpty().MinimumLength(2);
-            RuleFor(query => query.Model.DateOfBirthDay).NotEmpty().LessThan(DateTime.Now);
+            RuleFor(author => author.Name).NotEmpty().MinimumLength(2);
+            RuleFor(author => author.Surname).NotEmpty().MinimumLength(2);
+            RuleFor(author => author.DateOfBirthDay).NotEmpty().LessThan(DateTime.Now);
         }
     }
 }

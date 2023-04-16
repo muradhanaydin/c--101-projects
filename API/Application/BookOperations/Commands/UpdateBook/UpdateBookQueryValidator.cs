8af37e19e00a3975fb54ca5e68b5e07ff879d9fc
Muradhan Aydın <52311599+muradhanaydin@users.noreply.github.com>
2023-04-16 14:35:17 +0300

    Missing points have been corrected.

--- API/Application/BookOperations/Commands/UpdateBook/UpdateBookQueryValidator.cs
+++ API/Application/BookOperations/Commands/UpdateBook/UpdateBookQueryValidator.cs
@@ -2,15 +2,16 @@ using FluentValidation;
 
 namespace api.Application.BookOperations.Commands.UpdateBook
 {
-    public class UpdateBookQueryValidator : AbstractValidator<UpdateBookViewModal>
+    public class UpdateBookQueryValidator : AbstractValidator<UpdateBookQueryModel>
     {
         public UpdateBookQueryValidator()
         {
-            RuleFor(book => book.Category).NotEmpty();
-            RuleFor(book => book.Title).NotEmpty().MinimumLength(2);
-            RuleFor(book => DateTime.Parse(book.PublishedDate)).LessThan(DateTime.Now);
+            RuleFor(book => book.Title).NotEmpty().MinimumLength(2);            
+            RuleFor(book => book.Publisher).NotEmpty().MinimumLength(2);            
             RuleFor(book => book.PageCount).GreaterThan(0);
-            RuleFor(book => book.Publisher).NotEmpty();
+            RuleFor(book => book.AuthorId).GreaterThan(0);
+            RuleFor(book => book.CategoryId).GreaterThan(0);
+            RuleFor(book => DateTime.Parse(book.PublishedDate)).NotEmpty().LessThan(DateTime.Now);
         }
     }
 }
